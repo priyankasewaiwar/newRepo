e1a8bd1d5e25ea1610a9483e0e9a9ec1397badfb
priyankasewaiwar <priynkadohare@gmail.com>
2023-02-16 16:37:43 +0530

    DOM Assignment

--- dom.js
+++ dom.js
@@ -5,3 +5,8 @@ ulitems[2].style.backgroundColor = "green";
 for(var i=0; i<ulitems.length; i++){
     ulitems[i].style.fontWeight = "bold";
 }
+
+let inpTag = document.getElementsByTagName("li");
+for(var i=0;i<inpTag.length; i++){
+    inpTag[i].style.color = "blue";
+}
