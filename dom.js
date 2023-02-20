e81c60164b098cb0b29e6d5f542ec167e419b3f1
priyankasewaiwar <priynkadohare@gmail.com>
2023-02-20 16:10:47 +0530

    Add and Delete functionality

--- dom.js
+++ dom.js
@@ -11,11 +11,11 @@
 //     inpTag[i].style.color = "blue";
 // }
 
-let qs2 = document.querySelector(".list-group-item:nth-child(2)");
-qs2.style.backgroundColor = "lightgreen";
+// let qs2 = document.querySelector(".list-group-item:nth-child(2)");
+// qs2.style.backgroundColor = "lightgreen";
 
-let qs3 = document.querySelector(".list-group-item:nth-child(3)");
- qs3.style.display = 'none';
+// let qs3 = document.querySelector(".list-group-item:nth-child(3)");
+//  qs3.style.display = 'none';
 
 let items = document.querySelectorAll('.list-group-item');
 items[1].style.color = "green";
@@ -24,4 +24,50 @@ var odd = document.querySelectorAll('li:nth-child(odd)');
 for(var i=0;i<odd.length;i++){
     odd[i].style.backgroundColor = 'lightGrey';
 }
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+form.addEventListener('submit', addItem);
+
+itemList.addEventListener('click',deleteItem);
+
+function addItem(e){
+  e.preventDefault();
+  
+  var newItem = document.getElementById('item').value;
+
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(newItem));
+
+  var deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  deleteBtn.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteBtn);
+
+  var editBtn = document.createElement('button');
+  editBtn.className = 'btn btn-sm float-right edit';
+
+  editBtn.appendChild(document.createTextNode('Edit'));
+  li.appendChild(editBtn);
+
+  
+  itemList.appendChild(li);
+}
+
+
+function deleteItem(e){
+  e.preventDefault();
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are you sure ?')){
+      var liRem = e.target.parentElement;
+      itemList.removeChild(liRem);
+    }
+  }
+} 
+
+
+
 
